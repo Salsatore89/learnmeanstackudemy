f80b5b81a0b5a3b048ca2af99a9babfa67a42ce0
Alex Salvatore <asalvatorebenito@gmail.com>
2017-09-08 10:24:01 +0000

    another commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,20 @@
-var aloha = "Aloha Variables!";
-console.log(aloha);
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.static("src/views"));
+app.use(express.static("bower_components"));
+
+app.get("/", function(req, res){
+   res.send("Webah"); 
+});
+
+app.get("/routing", function(req, res){
+   res.send("Webah Routing"); 
+});
+
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log("The server is running on port: "+ port);
+});
